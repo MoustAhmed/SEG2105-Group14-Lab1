0cc58f4276be1a358901b6ceaba43b5eadfac255
dirir <dirir.ahmedd@gmail.com>
2025-11-17 18:55:34 -0500

    Completed the Junit Methods, used to test the methods in MathEval.java. Video has also been submitted in brightspace

--- MyCalculator1/app/src/test/java/com/example/mycalculator/ExampleUnitTest.java
+++ MyCalculator1/app/src/test/java/com/example/mycalculator/ExampleUnitTest.java
@@ -1,17 +1,31 @@
 package com.example.mycalculator;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
+
+    // PASSING TEST: Addition
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testAdditionPass() {
+        assertEquals("5", MathEval.eval("2+3"));
+    }
+
+    // FAILING TEST: Addition (intentional)
+    @Test
+    public void testAdditionFail() {
+        assertEquals("10", MathEval.eval("2+3"));
+    }
+
+    // PASSING TEST: Division
+    @Test
+    public void testDivisionPass() {
+        assertEquals("2", MathEval.eval("10/5"));
+    }
+
+    // FAILING TEST: Division (intentional)
+    @Test
+    public void testDivisionFail() {
+        assertEquals("5", MathEval.eval("10/0"));
     }
 }
